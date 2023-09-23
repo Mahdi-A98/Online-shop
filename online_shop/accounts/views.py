b1b6cf64f5884a19791ccdc66fd32721cb7810a8
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-23 07:41:35 +0330

    chore: register view

--- online_shop/accounts/views.py
+++ online_shop/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth.models import update_last_login
+from rest_framework.views import Response
+import jwt, datetime
+from .models import User
+from .serializer import UserSerializer
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        user_serializer = UserSerializer(data=request.data)
+        user_serializer.is_valid(raise_exception=True)
+        user_serializer.save()
+        return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+
