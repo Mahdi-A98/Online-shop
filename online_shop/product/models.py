c436128e9243d0a6dfffef5866b49242ee563f99
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-09-23 07:19:31 +0330

    chore: required imports

---
+++ online_shop/product/models.py
@@ -0,0 +1,8 @@
+# In the name of GOD
+
+from django.db import models
+from core.models import BaseModel
+from order.models import Discount
+import datetime.datetime as dt
+from datetime import timedelta
+
